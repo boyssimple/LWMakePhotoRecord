77c73326745a1236110bb8073168750949d117b1
luowei <luowei@cqyanyu.com>
2020-05-19 16:16:19 +0800

    0.0.4

---
+++ sources/LWMakePhotoRecord.h
@@ -0,0 +1,14 @@
+//
+//  LWMakePhotoRecord.h
+//  VideoRecord
+//
+//  Created by yanyu on 2020/5/19.
+//  Copyright © 2020 yanyu. All rights reserved.
+//
+
+#ifndef LWMakePhotoRecord_h
+#define LWMakePhotoRecord_h
+
+#import "LWVideoPictureViewController.h"
+
+#endif /* LWMakePhotoRecord_h */
